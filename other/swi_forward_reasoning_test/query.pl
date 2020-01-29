6d8969c6197cd25320da6c19597242d3f6f45572
Cristina Cornelio <cor@zurich.ibm.com>
2020-01-29 16:27:29 +0100

    updated results

---
+++ other/swi_forward_reasoning_test/query.pl
@@ -0,0 +1,3 @@
+:- initialization forall(p1(X,Y), writeln(p1(X,Y))).
+:- initialization forall(p0(A,B), writeln(p0(A,B))).
+:- initialization halt.
---
+++ other/swi_forward_reasoning_test/swipl_script.sh
@@ -0,0 +1,1 @@
+swipl facts.pl rules.pl query.pl > out_test_pl.txt 2>out_test_pl_err.txt
